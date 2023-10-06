8343d00e9447429cf89180836044be5a2afd4b57
yash <yashg4118@gmail.com>
2023-10-06 22:17:01 +0530

    second

---
+++ Lecture3 -IICipherShools/dtp.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=8;
+	int c=67;
+	char ch='a';
+	float b=3.33;
+	cout<<a<<" "<<ch<<" "<<b;
+	cout<<"\n"<<a+b<<"\n";
+	cout<<"a+c = "<<a+c;
+	return 0;
+}
